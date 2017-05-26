6cfb5d891d8be5ffc94f8a57923292b6f4fc1e9a
AminHP <mdan.hagh@gmail.com>
2017-05-26 14:57:01 +0430

    Remove java extra flags

--- project/modules/ijudge/scripts/java8/run.sh
+++ project/modules/ijudge/scripts/java8/run.sh
@@ -3,6 +3,4 @@
 CLASS_NAME=$(basename "${CODE_PATH%.*}")
 TARGET=$CLASS_NAME
 
-java -Djava.security.manager -Djava.security.policy=java.policy \
-	-Xmx1000g -DONLINE_JUDGE=true -Duser.language=en \
-	-Duser.region=US -Duser.variant=US -cp $COMPILED_DIR $TARGET
+java -Xmx1000g -DONLINE_JUDGE=true -cp $COMPILED_DIR $TARGET
