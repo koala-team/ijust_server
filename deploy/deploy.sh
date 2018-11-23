4e2696501c06645cccbf4d5f5a5f74c7853c5110
Salar Nasiri <salarnasiri@users.noreply.github.com>
2018-11-23 21:56:46 +0330

    Update deploy.sh to pull images from docker hub

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,11 @@
 #! /bin/sh
 
-docker build -f Dockerfile -t ijust ../
-docker build -f ../project/modules/ijudge/Dockerfile -t ijudge ../project/modules/ijudge/
+# docker build -f Dockerfile -t ijust ../
+# docker build -f ../project/modules/ijudge/Dockerfile -t ijudge ../project/modules/ijudge/
+
+docker pull salarn14/ijust:latest
+docker pull salarn14/ijudge:latest
+docker tag salarn14/ijust:latest ijust:latest
+docker tag salarn14/ijudge:latest ijudge:latest
+
 docker-compose up -d
